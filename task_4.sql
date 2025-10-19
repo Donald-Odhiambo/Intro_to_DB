21bdfacfb6c7436d5a79bae63962bb834099bedb
Oketch Donald  Odhiambo <primedohnix@yahoo.com>
2025-10-19 13:30:07 +0300

    Add task_4.sql for full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
--- Prints the full description of the table books
--- without using DESCRIBE or EXPLAIN
+-- Script that prints the full description of the table books
+-- from the database alx_book_store without using DESCRIBE or EXPLAIN
 
 SHOW CREATE TABLE books;
