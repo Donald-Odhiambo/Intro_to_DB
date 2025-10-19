1cd56dc1390c8ffd80befdab9bbbe73d78faed71
Oketch Donald  Odhiambo <primedohnix@yahoo.com>
2025-10-19 13:40:39 +0300

    Add task_4.sql for full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,8 @@
--- Script to print full description of books table without using DESCRIBE or EXPLAIN
--- Uses INFORMATION_SCHEMA.COLUMNS to get table structure details
-
+-- Get full description of books table without using DESCRIBE or EXPLAIN
 SELECT 
-    COLUMN_NAME,      -- Name of the column
-    DATA_TYPE,        -- Data type of the column  
-    IS_NULLABLE,      -- Whether column allows NULL values
-    COLUMN_DEFAULT    -- Default value of the column
-FROM 
-    INFORMATION_SCHEMA.COLUMNS  -- System table containing column metadata
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store'  -- Filter by database name
-    AND TABLE_NAME = 'books';        -- Filter by table name
+    COLUMN_NAME,
+    DATA_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
