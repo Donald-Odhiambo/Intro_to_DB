fc8c6135355c257b63c3490609c4ae66a62b2046
Oketch Donald  Odhiambo <primedohnix@yahoo.com>
2025-10-19 13:27:52 +0300

    Add task_4.sql for full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,4 @@
--- Get full description of books table
-SELECT 
-    COLUMN_NAME,
-    DATA_TYPE,
-    IS_NULLABLE,
-    COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME = 'books';
+-- Prints the full description of the table books
+-- without using DESCRIBE or EXPLAIN
+
+SHOW CREATE TABLE books;
